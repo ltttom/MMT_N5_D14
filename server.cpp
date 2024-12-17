7b40d4f097d0b18a1f4edc1e030c86bef1337d1b
ltttom <ctrang1705@gmail.com>
2024-12-17 08:03:42 +0000

    update new code

--- server.cpp
+++ server.cpp
@@ -1,77 +1,244 @@
+#include "server.hpp"
+
+#include <cstdint>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <iostream>
-#include <vector>
 #include <string>
-#include <thread>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <mutex> // Thêm thư viện mutex
-#pragma comment(lib, "ws2_32.lib")
+#include <vector>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <strings.h>
 
-using namespace std;
+#include <thread>   
+#include <mutex>
 
-#define SERVER_PORT 8080
-#define BUFFER_SIZE 1024
+#define CHUNK_SIZE 1024
 
-void handle_client(SOCKET client_socket, vector<pair<string, int>>& peer_list, mutex& peer_mutex) {
-    char buffer[BUFFER_SIZE] = {0};
-    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+std::vector<file_info> files;
+std::mutex mtx;
 
-    if (bytes_received <= 0) {
-        cerr << "Failed to receive data or client disconnected unexpectedly." << endl;
-        return;
+int server::caculate_chunk_number(uint32_t file_length){
+    int chunk_number = file_length / CHUNK_SIZE;
+    if((chunk_number * CHUNK_SIZE) < file_length){
+        chunk_number += 1;
     }
-
-    string peer_info(buffer);
-    size_t colon_pos = peer_info.find(":");
-    string ip = peer_info.substr(0, colon_pos);
-    int port = stoi(peer_info.substr(colon_pos + 1));
-
-    {
-        lock_guard<mutex> lock(peer_mutex);
-        peer_list.emplace_back(ip, port);
-        cout << "New peer registered: " << ip << ":" << port << endl;
-    }
-
-    // Gửi danh sách peer về cho client
-    string peer_list_str;
-    {
-        lock_guard<mutex> lock(peer_mutex);
-        for (const auto& peer : peer_list) {
-            peer_list_str += peer.first + ":" + to_string(peer.second) + ";";
-        }
-    }
-    send(client_socket, peer_list_str.c_str(), peer_list_str.size(), 0);
-
-    // Chờ thêm một thời gian để tránh mất kết nối quá sớm
-    this_thread::sleep_for(chrono::seconds(1));
-
-    closesocket(client_socket);
+    return chunk_number;
 }
 
-int main() {
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
+int server::register_reply(int process_fd, struct sockaddr_in client_address){
+    uint16_t number_of_file;
+    std::string file_name;
+    uint32_t file_length;
+    std::vector<file_info> temp_files = files;
+    std::string response = "registered";
 
-    SOCKET server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in server_addr = {0};
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    listen(server_socket, 10);
-
-    cout << "Server listening on port " << SERVER_PORT << endl;
-
-    vector<pair<string, int>> peer_list;
-    mutex peer_mutex; // Khởi tạo mutex để bảo vệ peer_list
-
-    while (true) {
-        SOCKET client_socket = accept(server_socket, NULL, NULL);
-        thread(handle_client, client_socket, ref(peer_list), ref(peer_mutex)).detach();
+    //client's address info
+    std::cout<<"client:"<< inet_ntoa(client_address.sin_addr)<<":"<<client_address.sin_port<<std::endl;
+    uint16_t port;
+    if(recv(process_fd, &port, sizeof(port), 0) < 0){
+        perror("client connection failed");
+        return -1;
     }
 
-    closesocket(server_socket);
-    WSACleanup();
+    if(recv(process_fd, &number_of_file, sizeof(number_of_file), 0) < 0){
+        perror("client connection failed");
+        return -1;
+    }
+
+    for(int i=0; i<number_of_file; i++){
+        if(recv(process_fd, &file_name, sizeof(file_name), 0) < 0){
+            perror("client connection failed");
+            return -1;
+        }
+        if(recv(process_fd, &file_length, sizeof(file_length), 0)<0){
+            perror("client connection failed");
+            return -1;
+        }
+        std::cout<<"file name:"<<file_name<<", "<<"file length:"<<file_length<<"\n";
+
+        //register for all the chunks
+        struct peer temp_peer;
+        temp_peer.address = client_address.sin_addr.s_addr;
+        temp_peer.port = port;
+        int chunk_size = caculate_chunk_number(file_length);
+        
+        for(int j=0; j< chunk_size; j++){
+            temp_peer.chunks.push_back(j);
+        }
+        //fill in other data
+        struct file_info temp_file_info;
+        temp_file_info.file_name = file_name;
+        temp_file_info.file_length = file_length;
+        temp_file_info.peers.push_back(temp_peer);
+        temp_files.push_back(temp_file_info);
+    }
+    mtx.lock();
+    
+    files = temp_files;
+    mtx.unlock();
+
+    send(process_fd, &response, sizeof(response), 0);
+
+    close(process_fd);
+
+    return 0;
+}
+
+int server::file_list_reply(int process_fd){
+    uint16_t number_of_file = files.size();
+    send(process_fd, &number_of_file, sizeof(number_of_file), 0);
+    for(int i=0; i<number_of_file; i++){
+        send(process_fd, &files[i].file_name, sizeof(files[i].file_name), 0);
+        send(process_fd, &files[i].file_length, sizeof(files[i].file_length), 0);
+    }
+    close(process_fd);
+    return 0;
+}
+
+int server::location_reply(int process_fd){
+    std::string file_name;
+    recv(process_fd, &file_name, sizeof(file_name), 0);
+    int i;
+    for(i = 0; i < files.size(); i++){
+        if(files[i].file_name == file_name){
+            uint16_t number_of_address = files[i].peers.size();
+            send(process_fd, &number_of_address, sizeof(number_of_address), 0);
+            send(process_fd, &files[i].file_length, sizeof(files[i].file_length), 0);
+            for(int j=0; j<number_of_address; j++){
+                send(process_fd, &files[i].peers[j].address, sizeof(files[i].peers[j].address), 0);
+                send(process_fd, &files[i].peers[j].port, sizeof(files[i].peers[j].port), 0);
+                uint16_t number_of_chunks = files[i].peers[j].chunks.size();
+                send(process_fd, &number_of_chunks, sizeof(number_of_chunks), 0);
+                for(int k = 0; k < number_of_chunks; k++){
+                    send(process_fd, &files[i].peers[j].chunks[k], sizeof(files[i].peers[j].chunks[k]), 0);
+                }
+            }
+        }
+    }
+    if( i == files.size()){
+        uint16_t number_of_address = 0;
+        send(process_fd, &number_of_address, sizeof(number_of_address), 0);
+    }
+    close(process_fd);
+    return 0;
+}
+
+int server::chunk_register_reply(int process_fd, struct sockaddr_in client_address){
+    std::string file_name;
+    int index;
+    uint16_t port;
+    std::string response = "registered";
+
+    if(recv(process_fd, &file_name, sizeof(file_name), 0) < 0){
+        perror("client connection failed");
+        return -1;
+    }
+
+    if(recv(process_fd, &index, sizeof(index), 0) < 0){
+        perror("client connection failed");
+        return -1;
+    }
+
+    if(recv(process_fd, &port, sizeof(port), 0) < 0){
+        perror("client connection failed");
+        return -1;
+    }
+
+    std::vector<file_info> temp_files = files;
+    for(int i=0; i<temp_files.size(); i++){
+        if(file_name == temp_files[i].file_name){
+            std::cout<<"hello"<<std::endl;
+            int j = 0;
+            for(j=0; j<temp_files[i].peers.size(); j++){
+                if(temp_files[i].peers[j].address == client_address.sin_addr.s_addr && temp_files[i].peers[j].port == port){
+                    temp_files[i].peers[j].chunks.push_back(index);
+                    break;
+                }
+            }
+            if (j == temp_files[i].peers.size()){
+                struct peer temp_peer;
+                temp_peer.address = client_address.sin_addr.s_addr;
+                temp_peer.port = port;
+                temp_peer.chunks.push_back(index);
+                temp_files[i].peers.push_back(temp_peer);
+                break;
+            }
+        }
+    }
+    mtx.lock();
+    files = temp_files;
+    mtx.unlock();
+
+    send(process_fd, &response, sizeof(response), 0);
+    close(process_fd);
+
+    return 0;
+}
+
+void server::handle_request(int new_sock, uint32_t request_type, struct sockaddr_in client_address){
+    switch (request_type){
+        case 0:
+            register_reply(new_sock, client_address);
+            break;
+        case 1:
+            file_list_reply(new_sock);
+            break;
+        case 2:
+            location_reply(new_sock);
+            break;
+        case 3:
+            chunk_register_reply(new_sock, client_address);
+            break;
+        default:
+            break;
+    }
+    return;
+}
+
+int server::execute(void){
+    server myserver;
+    int process_fd;
+    int server_fd;
+    struct sockaddr_in server_address, client_address;
+    //create socket
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Failed to create socket"); 
+        exit(0);
+    }
+
+    //initailize address structure
+    bzero(&server_address, sizeof(server_address));
+    server_address.sin_family = PF_INET; 
+    server_address.sin_addr.s_addr = INADDR_ANY; 
+    server_address.sin_port = htons(SERVER_PORT); 
+
+    //bind socket
+    if(bind(server_fd, (struct sockaddr*) &server_address, sizeof(server_address)) < 0){
+        perror("Failed to bind"); 
+        exit(0);
+    }
+
+    //listen socket
+    if(listen(server_fd, 5) < 0){
+        perror("Failed to listen"); 
+        exit(0);
+    }
+
+
+    while(1){
+        int client_address_length = sizeof(client_address);
+        if((process_fd = accept(server_fd,(struct sockaddr*) &client_address, (socklen_t *) &client_address_length)) < 0){
+            perror("Failed to accept"); 
+            exit(0);
+        }
+        uint32_t request_type;
+        recv(process_fd, &request_type, sizeof(request_type), 0);
+        //use multi-thread to handle requests
+        std::thread tid(&server::handle_request, myserver, process_fd, request_type, client_address);
+        tid.detach();
+        //handle_request(process_fd, request_type, client_address);
+    }
     return 0;
 }
